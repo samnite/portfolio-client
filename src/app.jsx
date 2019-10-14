e3bd314775583917895c66bb04c322d43dd867f9
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-14 19:29:30 +0300

    added navbar with min styling

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './app.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Navbar from './components/layout/navbar';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-function App() {
-  return <div className="App">Hello portfolio</div>;
-}
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <div className="container">
+        <Navbar />
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className="nav-extended">
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">
+            Logo
+          </a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile-demo">
+        <li>
+          <a href="sass.html">Sass</a>
+        </li>
+        <li>
+          <a href="badges.html">Components</a>
+        </li>
+        <li>
+          <a href="collapsible.html">JavaScript</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
+
+export default Navbar;
