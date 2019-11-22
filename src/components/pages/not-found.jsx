ac2568604cf17b951320e0d35daaeae29823d2be
Alexander Gribenchenko <samnite87@gmail.com>
2019-11-22 19:18:27 +0300

    implement 'NotFound' page

--- src/components/pages/not-found.jsx
+++ src/components/pages/not-found.jsx
@@ -1,13 +1,28 @@
 import React, { useEffect } from "react";
 import { setMainPage } from "../../store/actions/projects-actions";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const StyledError = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 80vh;
+  align-items: center;
+  h1 {
+    color: #333;
+  }
+`;
 
 const NotFound = ({ setMainPage }) => {
   useEffect(() => {
     setMainPage(false);
     // eslint-disable-next-line
   }, []);
-  return <div>Page not Found</div>;
+  return (
+    <StyledError>
+      <h1>Page Not Found</h1>
+    </StyledError>
+  );
 };
 
 export default connect(
